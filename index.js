c265ceb14736ee40aef38886a4bcf81c39fb2a10
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-11-04 15:27:24 +0300

    updated

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    const base_url = "https://github.com/Dorisk830/KitchenWares/"; // Updated base URL
+    const base_url = "http://localhost:3000";
     console.log("API base URL:", base_url);
+
+    // Cart array to store selected products
+    const cart = [];
 
     // Function to fetch and display products
     function fetchProducts() {
@@ -10,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
             .then((data) => {
                 const productsContainer = document.querySelector(".products");
 
-                data.apiProduct.forEach((product) => {
+                data.apiProducts.forEach((product) => {
                     const productElement = document.createElement("div");
                     productElement.className = "product";
 
@@ -31,8 +33,47 @@ document.addEventListener("DOMContentLoaded", () => {
                     price.textContent = `$${product.price}`;
 
                     const cartButton = document.createElement("button");
-                    cartButton.className = "cart";
-                    cartButton.innerHTML = `Add to Cart <i class='bx bxs-cart-add'></i>`;
+                    cartButton.className = "cart-button";
+                    cartButton.textContent = "Add to Cart";
+
+                    cartButton.addEventListener("click", () => {
+                        // Check if the product is already in the cart
+                        const isInCart = cart.some((item) => item.id === product.id);
+
+                        if (!isInCart) {
+                            // Add the selected product to the cart array
+                            cart.push(product);
+
+                            // You can add more logic to update the UI or provide feedback
+
+                            // Update the cart total
+                            const cartTotal = document.getElementById("cart-total");
+                            const total = cart.reduce((acc, item) => acc + parseFloat(item.price.replace('@', '').replace(',', '')), 0);
+                            cartTotal.textContent = `Total: $${total.toFixed(2)}`;
+
+                            // Add a click event listener to the remove button
+                            const cartList = document.getElementById("cart-list");
+                            const cartItem = document.createElement("li");
+                            cartItem.innerHTML = `${product.title} - $${product.price} <button class="remove-cart-item">Remove</button>`;
+                            cartList.appendChild(cartItem);
+
+                            const removeButton = cartItem.querySelector(".remove-cart-item");
+                            removeButton.addEventListener("click", () => {
+                                // Remove the product from the cart
+                                const productIndex = cart.findIndex((item) => item.id === product.id);
+                                if (productIndex !== -1) {
+                                    cart.splice(productIndex, 1);
+                                }
+
+                                // Remove the cart item from the list
+                                cartList.removeChild(cartItem);
+
+                                // Update the cart total
+                                const newTotal = cart.reduce((acc, item) => acc + parseFloat(item.price.replace('@', '').replace(',', '')), 0);
+                                cartTotal.textContent = `Total: $${newTotal.toFixed(2)}`;
+                            });
+                        }
+                    });
 
                     productElement.appendChild(img);
                     productElement.appendChild(title);
@@ -49,110 +90,3 @@ document.addEventListener("DOMContentLoaded", () => {
     // Fetch and display products when the page loads
     fetchProducts();
 });
-
-
-
-
-
-
-// document.addEventListener("DOMContentLoaded", () => {
-
-// const base_url = "https://github.com/Dorisk830/KitchenWares/"
-// console.log("API base URL:", base_Url);
-
-// // Create- post
-// // Read- get/read
-// // Update- patch
-// // Delete- removes
-
-// function apiProducts() {
-//     function create(Path, data) {
-//         return fetch; (`${base_url} ${path}`, {
-//             method: "POST",
-//             body: JSON.stringify(data),
-//             headers: {
-//                 "content-type": "application/json"
-//             }
-//         }).then(response =>{
-//             if(response.status==200) return response.json();
-//             throw new error; (response.status-text)
-//         })
-//     }
-// // ......................
-
-// function read(Path, id) {
-//     const endpoint=path;
-//     if (id !==undefined)(
-//         endpoint =`$(path)/$(id)`
-//     )
-//     return fetch; (`${base_url} ${endpoint}`, {
-
-//         headers: {
-//             "content-type": "application/json"
-//         }
-//     }).then(response =>{
-//         if(response.staus==200) return response.json();
-//         throw new error; (response.status-text)
-//     })
-// }
-
-// // ..................
-// function Update(Path, data) {
-//     return fetch; (`${base_url} ${path}`, {
-//         method: "PATCH",
-//         body: JSON.stringify(data),
-//         headers: {
-//             "content-type": "application/json"
-//         }
-//     }).then(response =>{
-//         if(response.staus==200) return response.json();
-//         throw new error; (response.status-text)
-//     })
-// }
-// // .......................
-// function remove(Path) {
-//     return fetch; (`${base_url} ${path}`, {
-//         method: "DELETE",
-//         headers: {
-//             "content-type": "application/json"
-//         }
-//     }).then(response =>{
-//         if(response.staus==200) return response.json();
-//         throw new error; (response.status-text)
-//     })
-// }
-
-// }
-// // // ajax to fetch data from Json file
-// // // variable that holds all methods and properties of he object-new XMLHttpRequest
-// // let http = new XMLHttpRequest();
-// // http.open('get', 'products.json', true);
-
-// // http.send
-
-// // http.onload = function () {
-// //     if (this.readyState == 4 && this.status == 200) {
-// //         let products = JSON.parse(this.responseText)
-
-// //         let output = "";
-// //         for (let item of products) {
-// //             output += `
-// //             <div class= "product">
-// //             <img src ="${item.image}" alt="${item.image}">
-// //             <p class= "title">${item.title}</p>
-// //             <p class= "description">${item.description}</p>
-// //             <p class= "price">
-// //             <span>${item.price}</span>
-// //             <span>&euro;</span>
-
-// //             </p>
-// //             <p class ="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
-// //             </div>           
-            
-// //             `;
-// //         }
-
-// //         document.querySelector(".products").innerHTML = output;
-// //     }
-// // }
-// });

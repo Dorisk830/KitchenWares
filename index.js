6a0bd93f701dd8b832917139ef3f55d5d29f6a6d
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-11-05 21:15:17 +0300

    updated

--- index.js
+++ index.js
@@ -1,46 +1,137 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const base_url = "https://github.com/Dorisk830/KitchenWares/";
+    const base_url = "https://my-json-server.typicode.com/Dorisk830/KitchenWares/apiProducts";
     console.log("API base URL:", base_url);
+
+    // Shopping cart to store selected products
+    const cart = [];
+
+    // Function to update the cart display
+    function updateCartDisplay() {
+        const cartItems = document.getElementById("cart-items");
+        const totalPriceElement = document.getElementById("total-price");
+        cartItems.innerHTML = '';
+
+        let total = 0;
+
+        // Populate the cart items and calculate the total price
+        cart.forEach((product, index) => {
+            const cartItem = document.createElement("li");
+            cartItem.textContent = `${product.title} - Price: ${product.price}`;
+
+            // Add a button to remove items from the cart
+            const removeButton = document.createElement("button");
+            removeButton.textContent = "Remove";
+            removeButton.addEventListener("click", () => {
+                removeFromCart(index);
+            });
+
+            cartItem.appendChild(removeButton);
+            cartItems.appendChild(cartItem);
+
+            // Calculate the total price
+            total += parseFloat(product.price.replace(/[^0-9.]/g, ''));
+        });
+
+        totalPriceElement.textContent = `Total Price: Ksh${total.toFixed(2)}`;
+    }
+
+    // Function to handle the "Add to Cart" button click
+    function handleAddToCartClick(product) {
+        cart.push(product);
+        updateCartDisplay();
+        //alert(`Added to Cart: ${product.title}`);
+    }
+
+    // Function to remove items from the cart
+    function removeFromCart(index) {
+        cart.splice(index, 1);
+        updateCartDisplay();
+    }
+
+    // Function to handle the checkout process
+    function handleCheckout() {
+        // Simulate a transaction process (replace with your actual logic)
+        // If successful, display the success popup
+        showSuccessPopup();
+    }
+
+    // Function to show the success popup
+    function showSuccessPopup() {
+        const successPopup = document.getElementById("success-popup");
+        successPopup.style.display = "block";
+
+        // Close the popup when the "x" button is clicked
+        const closeBtn = successPopup.querySelector(".close");
+        closeBtn.addEventListener("click", () => {
+            successPopup.style.display = "none";
+            
+            // Clear the cart after successful transaction
+            cart.length = 0;
+            updateCartDisplay();
+        });
+    }
+
+    // Attach a click event handler to the checkout button
+    const checkoutButton = document.getElementById("checkout-button");
+    checkoutButton.addEventListener("click", handleCheckout);
 
     // Function to fetch and display products
     function fetchProducts() {
-        fetch(`${base_url}products.json`)
-            .then((response) => response.json())
+        fetch(base_url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
             .then((data) => {
-                const productsList = document.getElementById("products-list");
+                console.log(data);
 
-                data.apiProducts.forEach((product) => {
-                    const productElement = document.createElement("div");
-                    productElement.className = "product";
+                if (Array.isArray(data)) {
+                    const productsList = document.getElementById("products-list");
 
-                    const img = document.createElement("img");
-                    img.src = product.image_url;
-                    img.alt = product.title;
+                    data.forEach((product) => {
+                        const productElement = document.createElement("div");
+                        productElement.className = "product";
 
-                    const title = document.createElement("div");
-                    title.className = "title";
-                    title.textContent = product.title;
+                        const img = document.createElement("img");
+                        img.src = product.image_url;
+                        img.alt = product.title;
 
-                    const description = document.createElement("div");
-                    description.className = "description";
-                    description.textContent = product.description;
+                        const title = document.createElement("div");
+                        title.className = "title";
+                        title.textContent = product.title;
 
-                    const price = document.createElement("div");
-                    price.className = "price";
-                    price.textContent = product.price;
+                        const description = document.createElement("div");
+                        description.className = "description";
+                        description.textContent = product.description;
 
-                    const cartButton = document.createElement("button");
-                    cartButton.className = "cart-button";
-                    cartButton.textContent = "Add to Cart";
+                        const price = document.createElement("div");
+                        price.className = "price";
+                        price.textContent = `Price: ${product.price}`;
 
-                    productElement.appendChild(img);
-                    productElement.appendChild(title);
-                    productElement.appendChild(description);
-                    productElement.appendChild(price);
-                    productElement.appendChild(cartButton);
+                        const cartButton = document.createElement("button");
+                        cartButton.className = "cart-button";
+                        cartButton.textContent = "Add to Cart";
 
-                    productsList.appendChild(productElement);
-                });
+                        // Add a click event handler to the "Add to Cart" button
+                        cartButton.addEventListener("click", () => {
+                            handleAddToCartClick(product);
+                        });
+
+                        // Append all elements to the product container
+                        productElement.appendChild(img);
+                        productElement.appendChild(title);
+                        productElement.appendChild(description);
+                        productElement.appendChild(price);
+                        productElement.appendChild(cartButton);
+
+                        // Append the product container to the products list
+                        productsList.appendChild(productElement);
+                    });
+                } else {
+                    console.error("Invalid or missing data structure.");
+                }
             })
             .catch((error) => console.error("Error fetching products:", error));
     }
